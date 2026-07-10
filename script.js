87d5237cbb5ed2702f2e0ce80aefe900b5c316b4
TimAppleyard <appleyard.timothy@gmail.com>
2026-07-09 19:13:17 -0500

    Created boilerplate and basic math functions

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    if(num2 == 0){
+        return "You can't divde by 0"
+    } else {
+        return +(num1/num2).toFixed(5)
+    }
+}
