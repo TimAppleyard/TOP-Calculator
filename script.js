70416630630fe5c59fdac9c4322b55337d653bbf
TimAppleyard <appleyard.timothy@gmail.com>
2026-07-09 19:16:40 -0500

    Added an operate function

--- script.js
+++ script.js
@@ -14,8 +14,12 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     if(num2 == 0){
-        return "You can't divde by 0"
+        return "You can't divde by 0";
     } else {
-        return +(num1/num2).toFixed(5)
+        return +(num1/num2).toFixed(5);
     }
+}
+
+function operate(num1, operator, num2){
+    return operator(num1, num2);
 }
